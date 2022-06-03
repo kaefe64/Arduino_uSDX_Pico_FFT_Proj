61e874ebe3d1dc9ed67a10963a6e2313447b7c78
kaefe64 <kaefe64@gmail.com>
2022-06-03 08:27:38 -0300

    Improving Mods Library comments

--- Arduino_uSDX_Pico_FFT/Arduino_uSDX_Pico_FFT.ino
+++ Arduino_uSDX_Pico_FFT/Arduino_uSDX_Pico_FFT.ino
@@ -1,18 +1,24 @@
 
 /*
+ * Arduino_uSDX_Pico.ino
+ * uSDX_PICO running in Raspberry Pi Pico RP2040 with TFT Display compiled in Arduino IDE
+ * 
+ * Created: May 2022
+ * Author: Klaus Fensterseifer
+ * https://github.com/kaefe64/Arduino_uSDX_Pico_FFT_Proj) 
+ * 
+ * 
 
-  Arduino_uSDX_Pico.ino  
-  
-  
-  uSDX_PICO running in Raspberry Pi Pico RP2040 with Arduino IDE
-  https://github.com/kaefe64/Arduino_uSDX_Pico_FFT_Proj
-  
+
+Use  Boards Manager: Arduino Mbed OS RP2040 Boards
+Do not use EarlePhilhower library (it is just conflitant with Mbed)
+
+Lib used: TFT_eSPI by Bodmer
 
 
-
-File  ...ini.elf.uf2  generated at  /tmp/arduino_build_...
-On Ubuntu, to allow program direct from Arduino IDE to the Pico, run once:
+On Ubuntu, to allow program Pico direct from Arduino IDE, run once:
 ~/.arduino15/packages/arduino/hardware/mbed_rp2040/3.0.1$ sudo ./post_install.sh
+File  ...ini.elf.uf2  generated at  /tmp/arduino_build_...
 
 
  
@@ -22,7 +28,7 @@ Mods in the Library files to fit to the project:
 
 TFT_eSPI LIBRARY:
 ----------------------------------------------------
-Change Arduino/Libraries/TFT_eSPI/User_Setup_Select.h
+Change Arduino/libraries/TFT_eSPI/User_Setup_Select.h
 Comment:
 //#include <User_Setup.h>           // Default setup is root library folder
 UnComment:
@@ -33,7 +39,7 @@ On User_Setups/Setup60_RP2040_ILI9341.h
 Uncomment:
 #define ILI9341_DRIVER
 #define TFT_RGB_ORDER TFT_RGB  // Colour order Red-Green-Blue
-Choose the SPI pins (SPI1)
+Choose the SPI pins (SPI1):
 // For the Pico use these #define lines
 #define TFT_SPI_PORT 1   // 0=SPI  1=SPI1
 #define TFT_MISO  12  //0  RX
@@ -42,21 +48,33 @@ Choose the SPI pins (SPI1)
 #define TFT_CS   13  //20  // Chip select control pin
 #define TFT_DC   4  //18  // Data Command control pin
 #define TFT_RST  5  //19  // Reset pin (could connect to Arduino RESET pin)
-Choose the character fonts are going to be used
+//#define TFT_BL     // LED back-light
+//#define TOUCH_CS 21     // Chip select pin (T_CS) of touch screen
+
+Leave the fonts available:
 #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
 #define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
 #define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+//#define LOAD_FONT8N // Font 8. Alternative to Font 8 above, slightly narrower, so 3 digits fit a 160 pixel TFT
+#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+
+// Comment out the #define below to stop the SPIFFS filing system and smooth font code being loaded
+// this will save ~20kbytes of FLASH
+#define SMOOTH_FONT
 
 --------------------------------------------------------------
 For ILI9341 + RP2040,  change  TFT_eSPI/TFT_Drivers/ILI9341_Defines.h
 #define TFT_WIDTH  320  //240
 #define TFT_HEIGHT 240  //320
+(this was reported as a issue to TFT_eSPI github project, and can be solved on new releases, just put it back if your display looks turned 90 degrees)
 
 --------------------------------------------------------------
-For Wire/I2C
+For Wire/I2C (thanks to Bob W9RAN for corrections)
 look pins at  pins_arduino.h
 .arduino15/packages/arduino/hardware/mbed_rp2040/3.0.1/variants/RASPBERRY_PI_PICO
-change:   #define WIRE_HOWMANY (2)  //default 1
 change the pins for I2C0 and include the pins for I2C1:
 // Wire
 #define PIN_WIRE_SDA        (16u)
@@ -64,6 +82,11 @@ change the pins for I2C0 and include the pins for I2C1:
 #define PIN_WIRE_SDA1       (18u)
 #define PIN_WIRE_SCL1       (19u)
 
+#define WIRE_HOWMANY    (2)  //default was 1
+#define I2C_SDA            (digitalPinToPinName(PIN_WIRE_SDA))
+#define I2C_SCL            (digitalPinToPinName(PIN_WIRE_SCL))
+#define I2C_SDA1            (digitalPinToPinName(PIN_WIRE_SDA))
+#define I2C_SCL1            (digitalPinToPinName(PIN_WIRE_SCL))
 */
 
 #include "uSDR.h"
@@ -83,19 +106,19 @@ void setup() {
   
   //uSDX.h -> Serialx = Serial1   //UART0  /dev/ttyUSB0
   Serialx.begin(115200);  
-  //Serialx.begin(19200);  
+
 
   uint16_t tim = millis();
  
-  //delay(1000);   // required for Serial1 too
+  // some delay required for Serial1 too
   for(int i=0; i<10; i++)
   {
   //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   gpio_set_mask(1<<LED_BUILTIN);
-  delay(50);                       // wait for a second
+  delay(50);                       // wait
   //digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   gpio_clr_mask(1<<LED_BUILTIN);
-   delay(50);                       // wait for a second
+   delay(50);                       // wait
   }
     
   while (!Serialx)  //Caution!!  with Serial, if no USB-serial open, it will be stuck here
@@ -108,7 +131,7 @@ void setup() {
   delay(250);
   }
 
-  Serialx.println("Arduino uSDX Pico");
+  Serialx.println("Arduino uSDX Pico FFT");
   Serialx.println("\nSerial took " + String((millis() - tim)) + "ms to start");
 
 
